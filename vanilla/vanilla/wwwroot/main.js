a2c1ecd45a6f1bfe161afe2bdf6d76509b6c51fc
Il Ung <ilungj@gmail.com>
2017-11-27 20:10:56 -1000

    Initialize ASP.NET Web API and serve original application as static files
    We still use MVC in the application, but the view part will do nothing.
    It's important to serve the web application as static files since we
    don't want to add complexity to the code like data-binding. Plus, it
    would be counter-intuitive to deviate away from using pure, vanilla
    JavaScript code when the whole purpose of branching away to this version
    was to provide simple and maintainable code to the staffs of Office of
    Elections.

---
+++ vanilla/vanilla/wwwroot/main.js
@@ -0,0 +1,137 @@
+
+// Cache of moment object that contains all kinds of information about this moment in time
+var now = moment();
+
+document.getElementById('next').addEventListener('click', getNextMonth);
+document.getElementById('prev').addEventListener('click', getPrevMonth);
+
+testAjax();
+
+makeCalendar();
+updateMonth();
+
+function makeCalendar() {
+  // Calendar object
+  var calendar = getCalendar();
+  // Cache of the template HTML
+  var weekTemplateHtml = '<tr>{{week}}</tr>',
+      dayTemplateHtml = '<td>{{day}}</td>';
+  // Final HTML strings
+  var weekFinalHtml = '',
+      dayFinalHtml = '';
+
+  // Loop through array, add <tr> Week </tr> for each week of the month
+  calendar.forEach(function (week) {
+    // Reset the day final HTML variable
+    dayFinalHtml = '';
+    // Loop through the week array, add <td> Date </td> for each day of the week
+    week.forEach(function (day) {
+      dayFinalHtml += dayTemplateHtml.replace(/{{day}}/g, day.date());
+    });
+    weekFinalHtml += weekTemplateHtml.replace(/{{week}}/g, dayFinalHtml);
+  });
+
+  // Replace the HTML of the calendar with modified HTML string
+  document.getElementById('calendar').innerHTML = weekFinalHtml;
+}
+
+// Inspired by https://stackoverflow.com/a/39803848
+function getCalendar() {
+  // Array that will store calendar data
+  var calendar = [];
+  // Numeric value of starting week and ending week of the month, relative to the whole year
+  var startWeek = now.startOf('month').week(),
+      endWeek = now.endOf('month').week();
+
+  // December is an exception: manually set endWeek to 52 otherwise it will set it to 1
+  if (now.month() === 11) {
+    endWeek = 52;
+  }
+
+  for (var week = startWeek; week <= endWeek; week++) {
+    calendar.push(
+        (new Array(7)).fill(0).map(function (n, i) {
+          return moment().week(week).startOf('week').clone().add(n + i, 'day');
+        })
+    );
+  }
+
+  return calendar;
+}
+
+function getNextMonth() {
+  now.add(1, 'months');
+  makeCalendar();
+  updateMonth();
+}
+
+function getPrevMonth() {
+  now.subtract(1, 'months');
+  makeCalendar();
+  updateMonth();
+}
+
+function updateMonth() {
+  console.log('updateMonth called', now.format('MMMM'));
+  document.getElementById('month').textContent = now.format('MMMM');
+}
+
+function testAjax() {
+  console.log('testAjax called');
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      console.log('xmlhttprequest done', 'xhr.status:', xhr.status);
+
+      // On success
+      if (xhr.status == 200) {
+        // Convert JSON string response to an Object
+        // var dataObject = JSON.parse(xhr.responseText);
+
+        // document.getElementById("list").innerHTML = listCreateHtml(dataObject);
+        document.getElementById('test-ajax').innerHTML = xhr.responseText;
+      }
+    }
+  }
+
+  xhr.open("GET", "/api/values/5", true);
+  xhr.send();
+}
+
+/*
+var template = document.getElementById("template-list-item");
+var templateHtml = template.innerHTML;
+
+// Ajax Call
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == XMLHttpRequest.DONE) {
+    // On success
+    if (xhr.state == 200) {
+      // Convert JSON string response to an Object
+      var dataObject = JSON.parse(xhr.responseText);
+
+      document.getElementById("list").innerHTML = listCreateHtml(dataObject);
+    }
+  }
+}
+
+xhr.open("GET", "/url/to/get-data/", true);
+xhr.send();
+
+// Function to generate and returns the HTML.
+// Accepts an object as a parameter
+function listCreateHtml(dataObject) {
+  var listHtml = "";
+
+  for (key in dataObject) {\
+    listHtml += templateHtml.replace(/{{id}}/g, dataObject[key]["id"])
+                            .replace(/{{name}}/g, dataObject[key]["name"])
+                            .replace(/{{city}}/g, dataObject[key]["city"])
+                            .replace(/{{state}}/g, dataObject[key]["state"])
+                            .replace(/{{url}}/g, dataObject[key]["url"]);
+  }
+
+  return listHtml;
+}
+ */
