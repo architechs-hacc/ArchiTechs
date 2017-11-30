d63743ae5db5e87aa06e85f3efecac843bdb15fb
Il Ung <ilungj@gmail.com>
2017-11-30 06:34:05 -1000

    Add IDatabase interface

---
+++ vanilla/vanilla/Models/IDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    interface IDatabase
+    {
+        IEnumerable<Training> Get();
+        bool TryGet(int id, out Training training);
+        Training Add(Training training);
+        bool Delete(int id);
+        bool Update(Training training);
+    }
+}
