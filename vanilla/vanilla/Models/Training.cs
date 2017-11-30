409248fe20f9d2adefaa54e7608a94796f4f19b5
Il Ung <ilungj@gmail.com>
2017-11-30 06:28:07 -1000

    Add the Training model

---
+++ vanilla/vanilla/Models/Training.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Training
+    {
+        public Training() { }
+
+        public Training(string name, int year, int month, int day)
+        {
+            Name = name;
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+        public int Day { get; set; }
+    }
+}
